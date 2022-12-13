06fb071bd190a0b1ffd9e09c5bc2421b7573e911
yujin Na <olim920@naver.com>
2022-12-13 23:50:56 +0900

    Create l1.java
    단계별 1단계 1-6

---
+++ baekjoon/l1.java
@@ -0,0 +1,51 @@
+package baekjoon;
+import java.util.*;
+
+public class l1 {
+	public static void main(String args[]){
+		//1000. 
+		//		Scanner sc = new Scanner(System.in);
+		//		int a, b;
+		//		a = sc.nextInt();
+		//		b = sc.nextInt();
+		//		System.out.println(a + b);
+		//	
+		//1001.	
+		//		Scanner sc = new Scanner(System.in);
+		//		int a,b;
+		//		a = sc.nextInt();
+		//		b = sc.nextInt();
+		//		System.out.println(a-b);
+		//	}
+
+		//10998.
+		//		Scanner sc = new Scanner(System.in);
+		//		int a,b;
+		//		a = sc.nextInt();
+		//		b = sc.nextInt();
+		//		System.out.println(a*b);
+
+		//1008.
+		//		Scanner sc = new Scanner(System.in);
+		//		int a,b;
+		//		a =sc.nextInt();
+		//		b =sc.nextInt();
+		//		System.out.println(a/b);
+
+
+		//10869.
+		//		Scanner sc = new Scanner(System.in);
+		//		int a,b;
+		//		a= sc.nextInt();
+		//		b= sc.nextInt();
+		//		System.out.println(a+b);
+		//		System.out.println(a-b);
+		//		System.out.println(a*b);
+		//		System.out.println(a/b);
+		//		System.out.println(a%b);
+
+		//10926.
+
+
+	}
+}

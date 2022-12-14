00b852cd20fb9f4dd7727ca5eae665a73361ad8f
yujin Na <olim920@naver.com>
2022-12-14 21:04:27 +0900

    배열의 두배 만들기

--- programmers/Solution_1.java
+++ programmers/Solution_1.java
@@ -57,25 +57,35 @@ public class Solution_1 {
 	}
 	//if(num1 == num2) return 1;
 	//else return -1;
-	
-	
+
+
 	//분수의 덧셈
 	class Solution7 {
-	    public int[] solution(int denum1, int num1, int denum2, int num2) {
-	        int denum = denum1*num2 + denum2*num1;
-	        int num = num1*num2;
-	        int gcd = getGcd(denum,num); 
-	        int[] answer = {denum/gcd, num/gcd};
-	        return answer;
-	    }
+		public int[] solution(int denum1, int num1, int denum2, int num2) {
+			int denum = denum1*num2 + denum2*num1;
+			int num = num1*num2;
+			int gcd = getGcd(denum,num); 
+			int[] answer = {denum/gcd, num/gcd};
+			return answer;
+		}
 
-	    public int getGcd(int num1, int num2){
-	        if(num1 % num2 == 0) {
-	            return num2;
-	        }
-	        else{
-	            return getGcd(num2 , num1 % num2);
-	        }
-	    }
+		public int getGcd(int num1, int num2){
+			if(num1 % num2 == 0) {
+				return num2;
+			}
+			else{
+				return getGcd(num2 , num1 % num2);
+			}
+		}
+	}
+	//배열 두배 만들기
+	class Solution8{
+		public int[] solution(int[] numbers) {
+			int[] answer = new int [numbers.length];
+			for(int i=0; i<numbers.length; i ++) {
+				answer[i] = numbers[i] *2;
+			}
+			return answer;
+		}
 	}
 }

a74f481db6f1e8bd6ec77e5e48ea99b9ce6516d8
yujin Na <olim920@naver.com>
2022-12-17 16:58:45 +0900

    고양이 출력하기

--- baekjoon/단계별문제l1.java
+++ baekjoon/단계별문제l1.java
@@ -105,10 +105,16 @@ public class l1 {
 		//			}	
 		//			}
 
-	}
-}	
+		//10171.고양이
+		public class Main {
+			public static void main(String[] args) {
+
+				System.out.println("\\    /\\");
+				System.out.println(" )  ( ')");
+				System.out.println("(  /  )");
+				System.out.println(" \\(__)|");    
+
+			}
+		}
 
 
-
-}
-}

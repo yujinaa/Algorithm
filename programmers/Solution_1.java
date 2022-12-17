5f94252392237ba816eac567b9ad699153bced20
yujin Na <olim920@naver.com>
2022-12-17 15:13:42 +0900

    배열의 평균값

--- programmers/Solution_1.java
+++ programmers/Solution_1.java
@@ -135,3 +135,16 @@ public class Solution_1 {
 		    }
 		    return answer;
 		}
+		
+		//배열 평균값
+		//// numbers_len은 배열 numbers의 길이입니다.
+		class Solution13{
+		double solution(int numbers[], size_t numbers_len) {
+		    double answer=0;
+		    double sum = 0;
+		    for(int i =0;i< numbers_len;i++){
+		        sum+=numbers[i];
+		    }
+
+		    return answer=sum/numbers_len;
+		}

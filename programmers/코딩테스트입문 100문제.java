1631836be6523245e3ac9d8f302b4c0ce19c8c20
yujin Na <olim920@naver.com>
2022-12-19 21:55:48 +0900

    문자열뒤집기

--- programmers/코딩테스트입문 100문제.java
+++ programmers/코딩테스트입문 100문제.java
@@ -241,3 +241,14 @@ public class Solution_1 {
 							}
 						}
 
+						//문자열뒤집기
+						class Solution {
+							public String solution(String my_string) {
+								String answer = "";
+								for(int i=my_string.length()-1;i>=0;i--){
+									answer+=my_string.substring(i,i+1);
+								}
+								return answer;
+							}
+						}  
+

2ec78183612de00c1f6073dd0a7acbf003c0391a
yujin Na <olim920@naver.com>
2022-12-19 16:40:47 +0900

    9498.시험성적

--- baekjoon/단계별문제2_조건문.java
+++ baekjoon/단계별문제2_조건문.java
@@ -2,7 +2,7 @@ package baekjoon.단계별문제;
 import java.util.Scanner;
 
 public class 단계별문제2_조건문 {
-	
+
 	//1330.두 수 비교하기
 	public class Main {
 		public static void main(String args[]){
@@ -18,8 +18,26 @@ public class 단계별문제2_조건문 {
 			}
 		}
 	}
-	
-	
+	//9498.시험 성적
+	//시험 점수를 입력받아 90 ~ 100점은 A, 80 ~ 89점은 B, 70 ~ 79점은 C, 60 ~ 69점은 D, 나머지 점수는 F를 출력하는 프로그램을 작성하시오.
+	public class Main {
+		public static void main(String args[]){
+			Scanner input = new Scanner(System.in);
+			int a= input.nextInt();
+			if(a>=90) {
+				System.out.println("A");
+			}else if(a>=80) {
+				System.out.println("B");
+			}else if(a>=70) {
+				System.out.println("C");
+			}else if(a>=60) {
+				System.out.println("D");
+			}else {
+				System.out.println("F");
+			}
+		}
+	}
+
 
 }
 

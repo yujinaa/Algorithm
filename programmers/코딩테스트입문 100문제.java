bdd15b5faa24398e0f3181a8d7cb325c91d1e148
yujin Na <olim920@naver.com>
2022-12-19 21:00:48 +0900

    피자나눠먹기

--- programmers/코딩테스트입문 100문제.java
+++ programmers/코딩테스트입문 100문제.java
@@ -204,3 +204,17 @@ public class Solution_1 {
 							}
 						}
 
+
+						//피자나눠먹기
+						class Solution {
+							public int solution(int slice, int n) {
+								int answer = 0;
+								if(n%slice==0){
+									answer=n/slice;
+								}else{
+									answer=(n/slice)+1;
+								}
+								return answer; 
+							} 
+						}
+

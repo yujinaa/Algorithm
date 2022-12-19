484b252f9249ae1d7eee1690634a864211ee9709
yujin Na <olim920@naver.com>
2022-12-19 15:50:23 +0900

    25083.새싹

--- baekjoon/단계별문제l1.java
+++ baekjoon/단계별문제l1.java
@@ -3,119 +3,119 @@ import java.util.*;
 
 //public class l1 {
 //	public static void main(String args[]){
-		//1000. 
-		//		Scanner sc = new Scanner(System.in);
-		//		int a, b;
-		//		a = sc.nextInt();
-		//		b = sc.nextInt();
-		//		System.out.println(a + b);
-		//	
-		//1001.	
-		//		Scanner sc = new Scanner(System.in);
-		//		int a,b;
-		//		a = sc.nextInt();
-		//		b = sc.nextInt();
-		//		System.out.println(a-b);
-		//	}
+//1000. 
+//		Scanner sc = new Scanner(System.in);
+//		int a, b;
+//		a = sc.nextInt();
+//		b = sc.nextInt();
+//		System.out.println(a + b);
+//	
+//1001.	
+//		Scanner sc = new Scanner(System.in);
+//		int a,b;
+//		a = sc.nextInt();
+//		b = sc.nextInt();
+//		System.out.println(a-b);
+//	}
 
-		//10998.
-		//		Scanner sc = new Scanner(System.in);
-		//		int a,b;
-		//		a = sc.nextInt();
-		//		b = sc.nextInt();
-		//		System.out.println(a*b);
+//10998.
+//		Scanner sc = new Scanner(System.in);
+//		int a,b;
+//		a = sc.nextInt();
+//		b = sc.nextInt();
+//		System.out.println(a*b);
 
-		//1008.
-		//		Scanner sc = new Scanner(System.in);
-		//		int a,b;
-		//		a =sc.nextInt();
-		//		b =sc.nextInt();
-		//		System.out.println((double)a/b);
-
-
-		//10869.
-		//		Scanner sc = new Scanner(System.in);
-		//		int a,b;
-		//		a= sc.nextInt();
-		//		b= sc.nextInt();
-		//		System.out.println(a+b);
-		//		System.out.println(a-b);
-		//		System.out.println(a*b);
-		//		System.out.println(a/b);
-		//		System.out.println(a%b);
-
-		//10926	
-		//		Scanner sc = new Scanner(System.in);
-		//		String a;
-		//		a= sc.next();
-		//		if(a.equals(a)) {
-		//			System.out.println(a+"??!");
-		//		}else {
-		//			System.out.println(a);
-		//		}	
-
-		//18108
-		//public class l1 {
-		//			public static void main(String args[]){
-		//				Scanner sc = new Scanner(System.in);
-		//				int y = sc.nextInt();
-		//				int sy = (y - 544)+1;
-		//				System.out.println(sy);
-		//
-		//			}
-		//}
-
-		//3003
-		//public class l1 {
-		//			public static void main(String args[]){
-		//				Scanner sc = new Scanner(System.in);
-		//				int[] chess = {1,1,2,2,2,8};
-		//				int[] inputC = new int[chess.length];
-		//				for(int i = 0; i<inputC.length;i++) {
-		//					inputC[i] = sc.nextInt();
-		//					System.out.print(chess[i]-inputC[i] +" ");
-		//				}
-		//			}	
-		//}
+//1008.
+//		Scanner sc = new Scanner(System.in);
+//		int a,b;
+//		a =sc.nextInt();
+//		b =sc.nextInt();
+//		System.out.println((double)a/b);
 
 
-		//10430
-		//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
-		//		public class l1 {
-		//			public static void main(String args[]){
-		//				Scanner sc = new Scanner(System.in);
-		//				int A= sc.nextInt();
-		//				int B= sc.nextInt();
-		//				int C= sc.nextInt();
-		//				System.out.println( (A+B)%C );
-		//				System.out.println( (A%C + B%C)%C );
-		//				System.out.println( (A*B)%C );
-		//				System.out.println( (A%C * B%C)%C );
+//10869.
+//		Scanner sc = new Scanner(System.in);
+//		int a,b;
+//		a= sc.nextInt();
+//		b= sc.nextInt();
+//		System.out.println(a+b);
+//		System.out.println(a-b);
+//		System.out.println(a*b);
+//		System.out.println(a/b);
+//		System.out.println(a%b);
 
-		//2558.곱셈
-		//public class l1 {
-		//			public static void main(String args[]){
-		//				Scanner sc = new Scanner(System.in);
-		//				int a = sc.nextInt();
-		//				int b = sc.nextInt();
-		//				System.out.println(a*(b%10));
-		//				System.out.println(a*((b%100)/10));
-		//				System.out.println(a*(b/100));
-		//				System.out.println(a*b);
-		//			}	
-		//			}
+//10926	
+//		Scanner sc = new Scanner(System.in);
+//		String a;
+//		a= sc.next();
+//		if(a.equals(a)) {
+//			System.out.println(a+"??!");
+//		}else {
+//			System.out.println(a);
+//		}	
 
-		//10171.고양이
-		//		public class Main {
-		//			public static void main(String[] args) {
-		//
-		//				System.out.println("\\    /\\");
-		//				System.out.println(" )  ( ')");
-		//				System.out.println("(  /  )");
-		//				System.out.println(" \\(__)|");    
-		//
-		//			}
-		//		}
+//18108
+//public class l1 {
+//			public static void main(String args[]){
+//				Scanner sc = new Scanner(System.in);
+//				int y = sc.nextInt();
+//				int sy = (y - 544)+1;
+//				System.out.println(sy);
+//
+//			}
+//}
+
+//3003
+//public class l1 {
+//			public static void main(String args[]){
+//				Scanner sc = new Scanner(System.in);
+//				int[] chess = {1,1,2,2,2,8};
+//				int[] inputC = new int[chess.length];
+//				for(int i = 0; i<inputC.length;i++) {
+//					inputC[i] = sc.nextInt();
+//					System.out.print(chess[i]-inputC[i] +" ");
+//				}
+//			}	
+//}
+
+
+//10430
+//첫째 줄에 (A+B)%C, 둘째 줄에 ((A%C) + (B%C))%C, 셋째 줄에 (A×B)%C, 넷째 줄에 ((A%C) × (B%C))%C를 출력한다.
+//		public class l1 {
+//			public static void main(String args[]){
+//				Scanner sc = new Scanner(System.in);
+//				int A= sc.nextInt();
+//				int B= sc.nextInt();
+//				int C= sc.nextInt();
+//				System.out.println( (A+B)%C );
+//				System.out.println( (A%C + B%C)%C );
+//				System.out.println( (A*B)%C );
+//				System.out.println( (A%C * B%C)%C );
+
+//2558.곱셈
+//public class l1 {
+//			public static void main(String args[]){
+//				Scanner sc = new Scanner(System.in);
+//				int a = sc.nextInt();
+//				int b = sc.nextInt();
+//				System.out.println(a*(b%10));
+//				System.out.println(a*((b%100)/10));
+//				System.out.println(a*(b/100));
+//				System.out.println(a*b);
+//			}	
+//			}
+
+//10171.고양이
+//		public class Main {
+//			public static void main(String[] args) {
+//
+//				System.out.println("\\    /\\");
+//				System.out.println(" )  ( ')");
+//				System.out.println("(  /  )");
+//				System.out.println(" \\(__)|");    
+//
+//			}
+//		}
 //	}}	
 
 
@@ -125,13 +125,32 @@ import java.util.*;
 //( 0 )"""\
 //|"^"`    |
 //||_/=\\__|
+//public class l1 {
+//	public static void main(String[] args) {
+//		System.out.println("|\\_/|");
+//		System.out.println("|q p|   /}");
+//		System.out.println("( 0 )\"\"\"\\");
+//		System.out.println("|\"^\"'    |");    
+//		System.out.println("||_/=\\\\__|");    
+//	}
+//}
+
+
+//25083.새싹.
+//,r'"7
+//r`-_   ,'  ,/
+// \. ". L_r'
+//   `~\/
+//      |
+//      |
 public class l1 {
 	public static void main(String[] args) {
-		System.out.println("|\\_/|");
-		System.out.println("|q p|   /}");
-		System.out.println("( 0 )\"\"\"\\");
-		System.out.println("|\"^\"'    |");    
-		System.out.println("||_/=\\\\__|");    
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
 	}
 }
 

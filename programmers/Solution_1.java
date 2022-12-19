8f7af8a6723954fa701af1170ddb57108a0e908a
yujin Na <olim920@naver.com>
2022-12-19 19:29:19 +0900

    머쓱이보다 키 큰 사람

--- programmers/Solution_1.java
+++ programmers/Solution_1.java
@@ -180,4 +180,16 @@ public class Solution_1 {
 								return answer;
 							}
 						}
+						
+						//머쓱이보다 키 큰 사람
+						class Solution16 {
+						    public int solution(int[] array, int height) {
+						        int answer = 0;
+						        for(int i =0;i<array.length;i++)
+						        if(array[i]>height){
+						           answer++;
+						        }
+						         return answer;
+						    }
+						}
 

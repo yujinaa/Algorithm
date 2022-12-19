60f149f9e6c05d90f2d84708701f1a9319c39554
yujin Na <olim920@naver.com>
2022-12-19 17:11:11 +0900

    2753.윤년

--- baekjoon/단계별문제2_조건문.java
+++ baekjoon/단계별문제2_조건문.java
@@ -37,7 +37,20 @@ public class 단계별문제2_조건문 {
 			}
 		}
 	}
-
+	//2753.윤년
+	//윤년은 연도가 4의 배수이면서, 100의 배수가 아닐 때 또는 400의 배수일 때이다.
+	public class Main {
+		public static void main(String args[]){
+			Scanner input = new Scanner(System.in);
+			int year= input.nextInt();
+			if(year%4==0 && (year%100!=0 || year%400==0)) {
+				System.out.println("1");
+			}else {
+				System.out.println("0");
+			}
+			
+		}
+	}
 
 }
 

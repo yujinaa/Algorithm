6f420d45ef1d0491721ab6a26a4ea4aa667a7ddd
yujin Na <olim920@naver.com>
2022-12-19 16:10:06 +0900

    1130.두 수 비교하기

--- baekjoon/단계별문제2_조건문.java
+++ baekjoon/단계별문제2_조건문.java
@@ -1,5 +1,26 @@
 package baekjoon.단계별문제;
+import java.util.Scanner;
 
 public class 단계별문제2_조건문 {
+	
+	//1330.두 수 비교하기
+	public class Main {
+		public static void main(String args[]){
+			Scanner input = new Scanner(System.in);
+			int a= input.nextInt();
+			int b= input.nextInt();
+			if(a>b) {
+				System.out.println(">");
+			}else if(a<b) {
+				System.out.println("<");
+			}else {
+				System.out.println("==");
+			}
+		}
+	}
+	
+	
+
+}
 
 }

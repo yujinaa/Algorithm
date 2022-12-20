295eb2e7636469d0ba904ca6224ea8648c4b126c
yujin Na <olim920@naver.com>
2022-12-20 18:03:45 +0900

    최댓값 만들기

--- programmers/코딩테스트입문 100문제.java
+++ programmers/코딩테스트입문 100문제.java
@@ -250,5 +250,19 @@ public class Solution_1 {
 								}
 								return answer;
 							}
-						}  
+						}
+						//최댓값 만들기
+						int solution(int numbers[], size_t numbers_len) {
+							int answer = 0;
+							int max=0;
+							for(int i =0;i<numbers_len;i++){
+								for(int j =0;j<numbers_len;j++){
+									if(i!=j && numbers[i] *numbers[j]>max){
+										max=numbers[i]*numbers[j];
+									}
+								}          
+							}
+							answer=max;   
+							return answer;          
+						}
 

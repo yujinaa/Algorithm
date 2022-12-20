19c52e1cd05df7dbd327acbabe0bdd1e61d6d793
yujin Na <olim920@naver.com>
2022-12-20 13:57:00 +0900

    14681.사분면 고르기

--- baekjoon/단계별문제2_조건문.java
+++ baekjoon/단계별문제2_조건문.java
@@ -48,10 +48,27 @@ public class 단계별문제2_조건문 {
 			}else {
 				System.out.println("0");
 			}
-			
+
 		}
 	}
 
-}
+	//	14681.사분면 고르기
+	public class Main {
+		public static void main(String args[]){
+			Scanner input = new Scanner(System.in);
+			int x= input.nextInt();
+			int y= input.nextInt();
+			if(x>0 && y>0) {
+				System.out.println("1");
+			}else if(x<0 && y>0) {
+				System.out.println("2");
+			}else if(x<0 && y<0) {
+				System.out.println("3");
+			}else {
+				System.out.println("14");
+			}
+		}
+	}
+
 
 }

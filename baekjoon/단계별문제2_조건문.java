773a34b1145fcba1fa0ddf53bbb7df3c8811b1b0
yujin Na <olim920@naver.com>
2022-12-20 17:06:45 +0900

    2480.주사위 세개

--- baekjoon/단계별문제2_조건문.java
+++ baekjoon/단계별문제2_조건문.java
@@ -106,5 +106,58 @@ public class 단계별문제2_조건문 {
 
 		}
 	}
+	//	2480.주사위 세개
+	//	같은 눈이 3개가 나오면 10,000원+(같은 눈)×1,000원의 상금을 받게 된다. 
+	//	같은 눈이 2개만 나오는 경우에는 1,000원+(같은 눈)×100원의 상금을 받게 된다. 
+	//	모두 다른 눈이 나오는 경우에는 (그 중 가장 큰 눈)×100원의 상금을 받게 된다.  
+	import java.util.Scanner;
+	public class Main {
+		public static void main(String args[]){
+			Scanner input = new Scanner(System.in);
+			int a= input.nextInt();
+			int b= input.nextInt();
+			int c = input.nextInt();
+
+			if(a==b && b==c && a==c) {
+				System.out.println(10000 + (a*1000));
+
+			}else if(a==b || a==c) {
+				System.out.println(1000+(a*100));
+			}else if(b==c){
+				System.out.println(1000+(b*100));
+			}else {
+				System.out.println(Math.max(a, Math.max(b, c))*100);
+			}
+		}
+	}
+
+	//주사위 다른 풀이
+	import java.util.Scanner;
+	public class Main {
+		public static void main(String args[]){
+			Scanner input = new Scanner(System.in);
+			int a= input.nextInt();
+			int b= input.nextInt();
+			int c = input.nextInt();
+
+			if(a==b && a==c) {
+				System.out.println(10000 + (a*1000));
+
+			}else if(a==b&&a!=c || a==c&&a!=b) {
+				System.out.println(1000+(a*100));
+			}else if(b==c&&b!=a){
+				System.out.println(1000+(b*100));
+			}else {
+				int max=a;
+				if(max<b)
+					max=b;
+				if(max<c)
+					max=c;
+				System.out.println(max*100);
+			}
+		}
+	}
+
+
 
 }

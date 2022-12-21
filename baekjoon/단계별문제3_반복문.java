9dceb30523bc60218108fa928c9cc8c93eac6c97
yujin Na <olim920@naver.com>
2022-12-21 17:33:27 +0900

    25304.영수증

--- baekjoon/단계별문제3_반복문.java
+++ baekjoon/단계별문제3_반복문.java
@@ -40,3 +40,27 @@ public class Main {
 		}
 	}
 }
+//25304.	영수증
+//첫째 줄에는 영수증에 적힌 총 금액 X가 주어진다.
+//둘째 줄에는 영수증에 적힌 구매한 물건의 종류의 수 N이 주어진다.
+//이후 N개의 줄에는 각 물건의 가격 a와 개수 b가 공백을 사이에 두고 주어진다.
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int x= input.nextInt();
+		int n= input.nextInt();
+		int sum=0;
+		for(int i =1;i<=n;i++) {
+			int a= input.nextInt();
+			int b= input.nextInt();
+
+			sum += a*b;
+		}
+		if(sum==x) 
+			System.out.println("yes");
+		else 
+			System.out.println("no");
+	}
+}
+

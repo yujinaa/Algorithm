c3c96101f0cd47e0b71943515e77a1ada25eb825
yujin Na <olim920@naver.com>
2022-12-29 00:57:17 +0900

    10952	A+B - 5

--- baekjoon/단계별문제3_반복문.java
+++ baekjoon/단계별문제3_반복문.java
@@ -186,3 +186,19 @@ public class Main {
 		}
 	}    
 }
+//10952	A+B - 5
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[]){
+		Scanner in=new Scanner(System.in);
+		while(true){
+			int A=in.nextInt();
+			int B=in.nextInt();
+			if(A==0 && B==0){
+				in.close();
+				break;
+			}
+			System.out.println(A+B);
+		}
+	}
+}

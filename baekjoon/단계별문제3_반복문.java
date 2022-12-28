3cfe546f3cf2331051554b6a94df360bf99b0cb6
yujin Na <olim920@naver.com>
2022-12-29 00:12:26 +0900

    2439.별 찍기 - 2

--- baekjoon/단계별문제3_반복문.java
+++ baekjoon/단계별문제3_반복문.java
@@ -169,3 +169,20 @@ public class Main {
       }
   }    
 }
+//2439.별 찍기 - 2
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int n = input.nextInt();
+		for (int i=1; i<=n; i++) {
+			for(int j=1;j<=n-i;j++) {
+				System.out.print(" ");
+			}
+			for(int k =1;k<=i;k++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}    
+}

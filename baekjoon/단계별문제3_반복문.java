a06829d26df79899bd5169ca231498d8e4d188bd
yujin Na <olim920@naver.com>
2022-12-28 23:30:32 +0900

    11022.A+B - 8

--- baekjoon/단계별문제3_반복문.java
+++ baekjoon/단계별문제3_반복문.java
@@ -139,3 +139,18 @@ public class Main {
 		}
 	}    
 }
+//11022.	A+B - 8
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int t = input.nextInt();
+      
+      for (int i=1; i<=t; i++) {
+          int a = input.nextInt();
+          int b = input.nextInt();
+          
+          System.out.println("Case #"+i +": " + a + " + " + b +  " = " + (a+b));
+      }
+  }    
+}

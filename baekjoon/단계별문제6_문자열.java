0a0eae28a166af0c98cb9626601a0d66694dd51c
yujin Na <olim920@naver.com>
2023-01-10 15:37:32 +0900

    11654.아스키 코드

---
+++ baekjoon/단계별문제6_문자열.java
@@ -0,0 +1,20 @@
+package baekjoon.단계별문제;
+
+//11654	 아스키 코드
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		String s = input.next();
+		System.out.println((int)s.charAt(0));
+	}
+}
+
+import java.util.Scanner;
+public class Main {
+	public static void main(String args[]){
+		Scanner input = new Scanner(System.in);
+		int n = input.next().charAt(0);
+		System.out.println(n);
+	}
+}

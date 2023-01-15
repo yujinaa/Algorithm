9d87d9ca837ff9d52241567af56b5cbce50cc204
yujin Na <olim920@naver.com>
2023-01-15 23:15:52 +0900

    2675	 문자열 반복

--- baekjoon/단계별문제6_문자열.java
+++ baekjoon/단계별문제6_문자열.java
@@ -46,3 +46,23 @@ public class Main {
 	    }
 		}
 	}
+	//2675	 문자열 반복
+	import java.util.Scanner;
+	public class Main {
+		public static void main(String args[]){
+			Scanner input = new Scanner(System.in);
+			int s = input.nextInt();
+			for(int i = 0; i < s; i++) {
+				int count = input.nextInt();
+				String str = input.next();
+				
+				for(int j = 0; j < str.length(); j++) {  
+					for(int k = 0; k < count; k++) {
+						System.out.print(str.charAt(j));
+					}
+				}
+	            
+				System.out.println();
+			}
+		}
+	}

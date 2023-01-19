109cc519df26f01dd5608ed1e77390d675121294
yujin Na <olim920@naver.com>
2023-01-19 17:07:16 +0900

    옷가게 할인받기

--- programmers/코딩테스트입문 100문제.java
+++ programmers/코딩테스트입문 100문제.java
@@ -489,5 +489,16 @@ public class Solution_1 {
 						        return my_string.replace(letter, "");
 						    }
 						}
+						//옷가게 할인받기
+						int solution(int price) {
+						    if (price>=500000) {
+						        price *= 0.8;
+						    } else if (price>=300000) {
+						        price *= 0.9;
+						    } else if (price>=100000) {
+						        price *= 0.95;
+						    }
+						    return price;
+						}
 
 

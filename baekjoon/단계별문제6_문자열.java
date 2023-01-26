d3d13bf700a40ad440b411cad24d6469d5481cdb
yujin Na <olim920@naver.com>
2023-01-26 23:21:35 +0900

    1152. 단어의 개수

--- baekjoon/단계별문제6_문자열.java
+++ baekjoon/단계별문제6_문자열.java
@@ -91,3 +91,21 @@ public class Main {
 			sc.close();
 		}
 	}
+	//1152	 단어의 개수
+	import java.util.Scanner;
+	public class Main {
+		public static void main(String args[]){
+			Scanner input = new Scanner(System.in);
+			String word = input.nextLine(); //문자열 입력 
+			String[] arr = word.split(" "); //공백을 기준으로 나눠 배열에 저장
+			if (arr.length == 0) { //아무것도 입력하지 않았다면 배열의 크기가 0이므로 배열[0] 이런식으로 접근하면 에러가 뜨므로 아예 0을 프린트 해주고 종료
+				System.out.println(0);
+				System.exit(0); //프로그램 강제 종료
+			}
+			if (arr[0] == "") { //배열의 맨 앞이 공백이면 공백은 단어가 아니므로 길이 -1
+				System.out.println(arr.length - 1);
+			}  else { //정상적으로 길이 출력
+				System.out.println(arr.length);
+			}
+		}
+	}
